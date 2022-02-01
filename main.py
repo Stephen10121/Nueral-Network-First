7792fa01023361ae9a90c0e760fe04ee2edaed68
Stephen Gruzin <stephengrzn@gmail.com>
2022-01-31 16:19:31 -0800

    Working on number checker

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World")
+from network import NueralNetwork
+import numpy as np
+
+with np.load("mnist.npz") as data:
+    training_images = data["training_images"]
+    training_labels = data["training_labels"]
+
+layer_sizes = (784,5,10)
+
+net = NueralNetwork(layer_sizes)
+net.print_accuracy(training_images, training_labels)
