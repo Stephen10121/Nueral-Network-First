6fa60a50b62437f1f20fefda231fcac576e17cf3
Stephen Gruzin <stephengrzn@gmail.com>
2022-02-09 15:12:25 -0800

    Made another nueral network file

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 with np.load("mnist.npz") as data:
     training_images = data["training_images"]
     training_labels = data["training_labels"]
+    print(len(data["training_images"][0]))
 
 layer_sizes = (784,5,10)
 
